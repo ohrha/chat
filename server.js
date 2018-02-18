129a53d114dd5472094ad2c8c81084c1ed0bf2fd
Angular CLI <angular-cli@angular.io>
2018-02-19 12:10:05 +1300

    chore: initial commit from @angular/cli

---
+++ server.js
@@ -0,0 +1,27 @@
+const express    = require('express');
+const http       = require('http');
+const path       = require('path');
+const bodyParser = require('body-parser');
+const cors       = require('cors');
+const routes     = require('./routes/routes');
+
+
+var app = express();
+
+app.use(cors());
+app.use(express.static(__dirname,'dist'));
+app.use(bodyParser.json({limit:'50mb'}));
+app.use('/routes',routes);
+app.get('*',(req,res)=>{
+
+    res.sendFile(path.join(__dirname,'dist/index.html'))
+
+});
+const port = process.env.PORT || '3000';
+const server = http.createServer(app);
+server.listen(port,()=>console.log(
+
+    "Server Listening at "+port
+))
+
+
