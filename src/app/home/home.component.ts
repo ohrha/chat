435b2fc27f1ef334e951ceaaff6d9b103cd1127e
ohrha <trigramthree@gmail.com>
2018-02-19 15:54:33 +1300

    CHANGE-ROUTE-FOR-HEROKU

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { PushService } from '../push.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+recievedMessage:string;
+messages: string[] = [];
+
+  constructor( private pushService:PushService) {
+      this.pushService.channel.bind('my-event', (message) => {
+      console.log(message);
+      this.recievedMessage = message.message;
+      this.messages.push(message.message)
+      console.log(this.recievedMessage)
+    });
+   }
+
+message:string;
+  ngOnInit() {
+
+
+
+  }
+  sendMessage(message){
+    console.log(this.message)
+    let txtMessage = {
+      message: this.message
+
+    }
+    this.pushService.pushTrigger(txtMessage).subscribe(data=>{
+
+      console.log(data)
+
+    })
+  }
+
+}
---
+++ src/app/push.service.ts
@@ -0,0 +1,28 @@
+declare const Pusher: any;
+import { Injectable } from '@angular/core';
+import { Http,Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class PushService {
+
+  pusher:any;
+  channel:any;
+  constructor(private http: Http) {
+ this.pusher = new Pusher('f756d847238c8314427d', {
+      cluster: 'us2',
+      encrypted: true
+    });
+this.channel = this.pusher.subscribe('my-channel');
+    
+   }
+
+  pushTrigger(message){
+
+    let headers = new Headers();
+    headers.append('Content-type', 'application/json');
+    return this.http.post('/routes',message,{headers:headers})
+    .map(res=>res.json())
+
+  }
+
