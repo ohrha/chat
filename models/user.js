d263079730021b582d5bcf8a579e64437f98d7fb
ohrha <trigramthree@gmail.com>
2018-03-24 11:08:03 +1300

    ADD-UNIQUENESS-ENFORCEMENT-TO-USERS-MODEL

--- models/user.js
+++ models/user.js
@@ -4,8 +4,10 @@ const bcrypt = require('bcryptjs');
 const UserSchema = mongoose.Schema({
 
 
-    name: { type: String, required: true },
-    password: { type: String, required: true }
+    name: { type: String, required: true,   unique:true,
+         dropDups: true },
+    password: { type: String, required: true,   unique:true,
+         dropDups: true }
 
 
 })
